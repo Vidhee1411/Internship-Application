6350257a6f7369a57cf32b21bc2ec1aad650a17e
Vidhee1411 <58293207+Vidhee1411@users.noreply.github.com>
2021-10-17 19:57:32 -0400

    Adding WorkExperience method stubs

--- WorkExperience.java
+++ WorkExperience.java
@@ -1,3 +1,61 @@
+/**
+ * The WorkExperience class support the resume class to set the work experience.
+ * of the student.
+ * @author Vidhee Patel
+ */
 public class WorkExperience {
-    
+    private String jobTitle;  
+    private String company ;
+    private String dateRange;
+    private String description;
+
+    /**
+     * The default WorkExperience constructor creates an empty WorkExperience.
+     */
+    public WorkExperience(){
+        
+    }
+    /**
+     * The parameterized constructor creates the work experience and allows 
+     * the student to add it into resume
+     * @param jobTitle The job title
+     * @param company The company name
+     * @param dateRange The period of the work
+     * @param description Description of the work
+     */
+    public WorkExperience(String jobTitle, String company, String dateRange, String description) {
+
+    }
+
+    /**
+     * The editJobTitle method allows a user to edit the title of a job.
+     * @param newTitle the new job title.
+     */
+    public void editJobTitle(String newTitle) {
+
+    }
+
+    /**
+     * The editCompany method allows a user to edit the comapany of work.
+     * @param company the new company name.
+     */
+    public void editCompany(String company) {
+
+    }
+
+    /**
+     * The editDateRange method allows a user to edit the period of work.
+     * @param dateRange the new period of work.
+     */
+    public void editDateRange(String dateRange) {
+
+    }
+
+    /**
+     * The editDescription method allows a user to edit the description of the work..
+     * @param description the new description.
+     */
+    public void editDescription(String description) {
+
+    }
 }
