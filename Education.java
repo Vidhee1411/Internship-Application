2f8e8b76feceec8efaec2b9a4b93404f0662b1f8
Vidhee1411 <58293207+Vidhee1411@users.noreply.github.com>
2021-10-17 19:22:48 -0400

    Education class methods

--- Education.java
+++ Education.java
@@ -1,3 +1,77 @@
+/**
+ * The Education class support the resume class to set the education 
+ * of the student.
+ * @author Vidhee Patel
+ */
 public class Education {
+    private String nameofUniversity;
+    private String collegeMajor;
+    private double GPA;
+    private String expectedGradDate;
+
+    /**
+     * The setName method sets the name of University.
+     * @param name University name
+     */
+    public void setName(String name) {
+        this.nameofUniversity = name;
+    }
+
+    /**
+     * The getName method returns the name of the University.
+     * @return nameofUniversity
+     */
+    public String getName() {
+        return nameofUniversity;
+    }
+
+    /**
+     * The setMajor method sets the major of the student.
+     * @param major
+     */
+    public void setMajor(String major) {
+        this.collegeMajor = major;
+    }
     
+    /**
+     * The getMajor method returns the major of the student.
+     * @return collegeMajor
+     */
+    public String getMajor() {
+        return collegeMajor;
+    }
+
+    /**
+     * The setGPA method sets the GPA of the student.
+     * @param gpa
+     */
+    public void setGPA(double gpa) {
+        this.GPA = gpa;
+    }
+
+    /**
+     * The getGPA method return the gpa of the student.
+     * @return
+     */
+    public double getGPA() {
+        return GPA;
+    }
+
+    /**
+     * The setExpectedGradDate setd the expected graduation date.
+     * @param date
+     */
+    public void setExpectedGradDate(String date)
+    {
+        this.expectedGradDate = date;
+    }
+
+    /**
+     * The getExpectedGradDate returns the expected graduation date.
+     * @return expectedGradDate
+     */
+    public String getExpectedGradDate() {
+        return expectedGradDate;
+    }
+
 }
