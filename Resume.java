5afd9d307d7b1edad7e9969b74be37a616913ab5
Vidhee1411 <58293207+Vidhee1411@users.noreply.github.com>
2021-10-18 02:01:31 -0400

    Adding Resume method stubs

--- Resume.java
+++ Resume.java
@@ -1,7 +1,69 @@
 /**
- * The Student class creates a student account in the internship system.
+ * The resume class creates a resume of the student in the internship system.
  * @author Vidhee Patel
  */
+import java.util.ArrayList;
+
 public class Resume {
+    private String studentFirstName;
+    private String studentLastName;
+    private String studentUSCEmail;
+    private String yearInSchool; 
+    private ArrayList<String> skills;
+    private ArrayList<String> classes;
+    private ArrayList<Education> education;
+    private ArrayList<WorkExperience> workExperiences;  
+
+    public Resume() {
+
+    }
+
+    public Resume(String firstName, String lastName, String email, String yearInSchool) {
+
+    }
+
+    public void editFirstName(String firstname) {
+
+    }
+
+    public void editLastName(String lastname) {
+
+    }
+
+    public void editEmail(String email) {
+
+    }
+
+    public void editYearInSchool (String year) {
+
+    }
+
+    public void addClass(String classname) {
+
+    }
+
+    public void addExperience(WorkExperience experience) {
+
+    }
+
+    public void addEducation() {
+
+    }
+
+    public void removeSkill(String skill) {
+
+    }
+
+    public void removeClass(String classname) {
+
+    }
+
+    public void removeExperience(WorkExperience experience) {
+
+    }
+
+    public void removeEducation() {
+
+    }
     
 }
