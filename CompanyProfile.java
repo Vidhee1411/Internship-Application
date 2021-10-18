f79b9e92690f6f647246bf9f959a21d546269d06
Vidhee1411 <58293207+Vidhee1411@users.noreply.github.com>
2021-10-18 01:41:53 -0400

    Adding company profile method stubs

--- CompanyProfile.java
+++ CompanyProfile.java
@@ -1,3 +1,50 @@
+/**
+ * The class creates a company profile in the internship system.
+ * @author Vidhee Patel
+ */
+import java.util.ArrayList;
+
 public class CompanyProfile {
-    
+    private String companyName;
+    private String hqAddress;
+    private String description;
+    private ArrayList<JobListing> listings;
+    private ArrayList<Review> reviewList;
+
+    public CompanyProfile(String companyName, String hqAddress, String description){
+
+    }
+
+    public String getCompanyName() {
+        return "";
+    }
+
+    public  void setCompanyName(String name) {
+
+    }
+
+    public String displayReviews() {
+        return "";
+    }
+
+    public String getDescription() {
+        return "";
+    }
+
+    public void setDescription() {
+
+    }
+
+    public void addListing() {
+
+    }
+
+    public ArrayList<JobListing> getListings() {
+        return null;
+    }
+
+    public boolean removeListing() {
+        return true;
+    }
+
 }
