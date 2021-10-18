898fa36014ad45c4c9899da4b0c146e2ed53a20a
Vidhee1411 <58293207+Vidhee1411@users.noreply.github.com>
2021-10-17 23:13:08 -0400

    Adding administrator method stubs

--- Administrator.java
+++ Administrator.java
@@ -1,3 +1,23 @@
 public class Administrator {
     
+    public Administrator(String firstName, String lastName, String email, String password, int permission) {
+
+    }
+
+    public void hideReview(Review review) {
+
+    }
+
+    public boolean removeListing(CompanyProfile company) {
+        return true;
+    }
+    
+    public void removeAccount(User user) {
+
+    }
+
+    public void registerAccount(String firstName, String lastName, String email, String password) {
+        
+    }
+
 }
