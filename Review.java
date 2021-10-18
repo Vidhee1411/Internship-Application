c293e0acbe58b2e7b479aa544b85458ce3f1819c
Vidhee1411 <58293207+Vidhee1411@users.noreply.github.com>
2021-10-17 20:04:01 -0400

    Adding review method stubs

--- Review.java
+++ Review.java
@@ -1,3 +1,23 @@
 public class Review {
-    
+    private String reviewFirstName;
+    private String reviewLastName;
+    private int rating;
+    private String message;
+    private boolean hidden;
+
+    public Review() {
+
+    }
+
+    public Review(String firstName, String lastName, int rating, String message) {
+
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    public void toggleVisibility() {
+
+    }
 }
