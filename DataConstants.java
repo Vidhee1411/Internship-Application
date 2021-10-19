39ec5865bc7d3819e480d42b571f8329b8db1b57
Zt572 <ztyoung@email.sc.edu>
2021-10-19 18:05:15 -0400

    Capitalizing constant names

--- DataConstants.java
+++ DataConstants.java
@@ -7,95 +7,95 @@ public class DataConstants {
     /**
      * file names 
      */
-    protected static final String student_File_Name = "JSON\\Student.json";
-    protected static final String admin_File_Name = "JSON\\Administrator.json";
-    protected static final String employer_File_Name = "JSON\\employers.json";
-    protected static final String company_Profile_File_Name = "JSON\\companyProfiles.json";
-    protected static final String job_Listing_File_Name = "JSON\\jobListings.json";
-    protected static final String resume_File_Name = "JSON\\Resume.json";
+    protected static final String STUDENT_FILE_NAME = "JSON\\Student.json";
+    protected static final String ADMIN_FILE_NAME = "JSON\\Administrator.json";
+    protected static final String EMPLOYER_FILE_NAME = "JSON\\employers.json";
+    protected static final String COMPANY_PROFILE_FILE_NAME = "JSON\\companyProfiles.json";
+    protected static final String JOB_LISTING_FILE_NAME = "JSON\\jobListings.json";
+    protected static final String RESUME_FILE_NAME = "JSON\\Resume.json";
     /**
      * admin fields
      */
-    protected static final String admin_ID = "userID";
-    protected static final String admin_First_Name = "firstName";
-    protected static final String admin_Last_Name = "lastName";
-    protected static final String admin_Email = "email";
-    protected static final String admin_Password = "password";
-    protected static final String admin_Permission = "Permission";
+    protected static final String ADMIN_ID = "userID";
+    protected static final String ADMIN_FIRST_NAME = "firstName";
+    protected static final String ADMIN_LAST_NAME = "lastName";
+    protected static final String ADMIN_EMAIL = "email";
+    protected static final String ADMIN_PASSWORD = "password";
+    protected static final String ADMIN_PERMISSION = "Permission";
     /**
      * student fields 
      */
-     protected static final String student_ID = "userID";
-     protected static final String student_First_Name = "firstName";
-     protected static final String student_Last_Name = "lastName";
-     protected static final String student_Email = "email";
-     protected static final String student_Password = "password";
-     protected static final String student_permission = "Permission";
-     protected static final String student_Reviews = "reviews";
-     protected static final String student_Resume_ID = "resume";
+     protected static final String STUDENT_ID = "userID";
+     protected static final String STUDENT_FIRST_NAME = "firstName";
+     protected static final String STUDENT_LAST_NAME = "lastName";
+     protected static final String STUDENT_EMAIL = "email";
+     protected static final String STUDENT_PASSWORD = "password";
+     protected static final String STUDENT_PERMISSION = "Permission";
+     protected static final String STUDENT_REVIEWS = "reviews";
+     protected static final String STUDENT_RESUME_ID = "resume";
      /**
       * employer fields 
       */
-      protected static final String employer_ID = "userID";
-     protected static final String employer_First_Name = "firstName";
-     protected static final String employer_Last_Name = "lastName";
-     protected static final String employer_Email = "email";
-     protected static final String employer_Password = "password";
-     protected static final String employer_ = "Permission";
-     protected static final String employer_Reviews = "reviews";
-     protected static final String employer_Associated_Company = "associatedCompanyID";
+      protected static final String EMPLOYER_ID = "userID";
+     protected static final String EMPLOYER_FIRST_NAME = "firstName";
+     protected static final String EMPLOYER_LAST_NAME = "lastName";
+     protected static final String EMPLOYER_EMAIL = "email";
+     protected static final String EMPLOYER_PASSWORD = "password";
+     protected static final String EMPLOYER_PERMISSION = "Permission";
+     protected static final String EMPLOYER_REVIEWS = "reviews";
+     protected static final String EMPLOYER_ASSOCIATED_COMPANY = "associatedCompanyID";
      /**
       * company profile fields 
       */
-      protected static final String company_ID = "companyID";
-      protected static final String company_HQ_Adress = "hqAdress";
-      protected static final String company_Name = "companyName";
-      protected static final String company_Description = "description";
-      protected static final String company_Listings_IDS = "currentListingIDs";
-      protected static final String company_Associated_Employer_IDs = "associatedEmployersIDs";
-      protected static final String company_Reviews = "reviews";
+      protected static final String COMPANY_ID = "companyID";
+      protected static final String COMPANY_HQ_ADRESS = "hqAdress";
+      protected static final String COMPANY_NAME = "companyName";
+      protected static final String COMPANY_DESCRIPTION = "description";
+      protected static final String COMPANY_LISTINGS_IDS = "currentListingIDs";
+      protected static final String COMPANY_ASSOCIATED_EMPLOYER_IDS = "associatedEmployersIDs";
+      protected static final String COMPANY_REVIEWS = "reviews";
     /**
      * joblisting fields
      */
-    protected static final String listing_ID = "listingID";
-    protected static final String listing_Title = "title";
-    protected static final String listing_Paid = "paid";
-    protected static final String listing_Pay_Rate = "payRate";
-    protected static final String listing_Required_Skills = "requiredSkills";
-    protected static final String listing_Company_Name = "companyName";
-    protected static final String listing_Hidden = "hidden";
-    protected static final String listing_Applicant_IDS = "applicantIDs";
+    protected static final String LISTING_ID = "listingID";
+    protected static final String LISTING_TITLE = "title";
+    protected static final String LISTING_PAID = "paid";
+    protected static final String LISTING_PAY_RATE = "payRate";
+    protected static final String LISTING_REQUIRED_SKILLS = "requiredSkills";
+    protected static final String LISTING_COMPANY_NAME = "companyName";
+    protected static final String LISTING_HIDDEN = "hidden";
+    protected static final String LISTING_APPLICANT_IDS = "applicantIDs";
     /**
      * resume fields
      */
-    protected static final String resume_User_ID = "userID";
-    protected static final String resume_School_year = "yearInSchool";
-    protected static final String resume_Skills = "Skills";
-    protected static final String resume_Classes = "classes";
-    protected static final String resume_Work_Experience = "workexperiences";
-    protected static final String resume_Education = "education";
+    protected static final String RESUME_USER_ID = "userID";
+    protected static final String RESUME_SCHOOL_YEAR = "yearInSchool";
+    protected static final String RESUME_SKILLS = "Skills";
+    protected static final String RESUME_CLASSES = "classes";
+    protected static final String RESUME_WORK_EXPERIENCE = "workexperiences";
+    protected static final String RESUME_EDUCATION = "education";
     /**
      * work experience fields 
      */
-    protected static final String experience_Title = "title";
-    protected static final String experience_Company = "Company";
-    protected static final String experience_Date_Range = "dateRange";
-    protected static final String experience_Description = "description";
+    protected static final String EXPERIENCE_TITLE = "title";
+    protected static final String EXPERIENCE_COMPANY = "Company";
+    protected static final String EXPERIENCE_DATE_RANGE = "dateRange";
+    protected static final String EXPERIENCE_DESCRIPTION = "description";
     /**
      * education fields
      */
-    protected static final String education_University_Name = "universityName";
-    protected static final String education_Major = "collegeMajor";
-    protected static final String education_GPA = "GPA";
-    protected static final String eudcation_Graduation_Date = "expectedGradDate";
+    protected static final String EDUCATION_UNIVERSITY_NAME = "universityName";
+    protected static final String EDUCATION_MAJOR = "collegeMajor";
+    protected static final String EDUCATION_GPA = "GPA";
+    protected static final String EDUCATION_GRADUATION_DATE = "expectedGradDate";
     /**
      *  review fields 
      */
-    protected static final String review_First_Name = "firstName";
-    protected static final String review_Last_Name = "lastName";
-    protected static final String review_Rating = "rating";
-    protected static final String review_Message = "message";
-    protected static final String review_Hidden = "hidden";
+    protected static final String REVIEW_FIRST_NAME = "firstName";
+    protected static final String REVIEW_LAST_NAME = "lastName";
+    protected static final String REVIEW_RATING = "rating";
+    protected static final String REVIEW_MESSAGE = "message";
+    protected static final String REVIEW_HIDDEN = "hidden";
    
 
 
