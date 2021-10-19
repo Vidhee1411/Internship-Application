e4bf1aa58c0760890d7b7c3bef32d46e1e5b97ba
JoshuaDuPuis <jdupuis@email.sc.edu>
2021-10-19 09:42:45 -0400

    Implemented the user class constructor

--- Student.java
+++ Student.java
@@ -9,11 +9,7 @@ public class Student extends User{
     private ArrayList<Review> reviewsFromCompanies;
 
     public Student(String firstName, String lastName, String email, String password, int permission) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.permission = permission;
+        super(firstName, lastName, email, password, permission);
     }
     
     public void createResume() {
