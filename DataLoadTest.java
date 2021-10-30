8b1e36327e4fcfeafa806f566ab5f29577cf7ebc
unknown <brentph@email.sc.edu>
2021-10-30 10:15:52 -0400

    for some reason sort by payrate was not pushed to git

--- DataLoadTest.java
+++ DataLoadTest.java
@@ -7,8 +7,7 @@ public class DataLoadTest {
     public static void main(String[] args){ 
         SearchableDatabase a = DataLoader.loadData();
         ArrayList<User> temp = a.getUsers();
-        Student student = (Student)temp.get(2);
-        student.getResume().toFile();
+        
 
     }
 }
