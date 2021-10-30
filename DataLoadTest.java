d076df09501f11d6951d1f5b0b61479a38834382
unknown <brentph@email.sc.edu>
2021-10-30 17:12:34 -0400

    fixed Json files

--- DataLoadTest.java
+++ DataLoadTest.java
@@ -5,12 +5,10 @@ import java.util.Map;
 public class DataLoadTest {
     
     public static void main(String[] args){ 
-        
-        SearchableDatabase a = DataLoader.loadData();
-        ArrayList<User> temp = a.getUsers();
-        User rm = temp.get(3);
-        Administrator acc = (Administrator) temp.get(0);
-        acc.removeAccount(rm);
+        SearchableDatabase database = DataLoader.loadData();
+        InternshipApplication temp = new InternshipApplication();
+        InternshipUI tempUI = new InternshipUI();
+        tempUI.run();
         
 
     }
