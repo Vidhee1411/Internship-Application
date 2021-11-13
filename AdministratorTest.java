c82913471cc5bb2c0eceb06412251175809c7672
JoshuaDuPuis <jdupuis@email.sc.edu>
2021-11-13 15:51:59 -0500

    Committing the rest of the admin class tests

--- AdministratorTest.java
+++ AdministratorTest.java
@@ -3,6 +3,7 @@ import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import java.lang.NullPointerException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -69,4 +70,74 @@ public class AdministratorTest {
             assertNotNull(null);
         }
     }
+
+    @Test
+    public void testRemoveValidStudent() {
+        Student stud = new Student("Joe", "Clash", "jclash@email.sc.edu", "pleaseNotNow", "senior");
+        searchableDatabase.addUser(stud);
+        admin.removeAccount(stud);
+        assertEquals(0, searchableDatabase.getUsers().size());
+    }
+
+    @Test
+    public void testRemoveValidStudentTwice() {
+        Student stud = new Student("Joe", "Clash", "jclash@email.sc.edu", "pleaseNotNow", "senior");
+        searchableDatabase.addUser(stud);
+        admin.removeAccount(stud);
+        try {
+        admin.removeAccount(stud);
+        int i = 0;
+        assertEquals(1, i);
+        } catch (NullPointerException e){
+            assertNull(null);
+        }
+    }
+
+    @Test
+    public void testRemoveNullStudent() {
+        Student stud = null;
+        searchableDatabase.addUser(stud);
+        admin.removeAccount(stud);
+        assertEquals(0, searchableDatabase.getUsers().size());
+    }
+
+    @Test
+    public void testRemoveValidEmployer() {
+        Employer emp = new Employer("Joe", "Washing", "j@jman.org", "organized");
+        searchableDatabase.addUser(emp);
+        admin.removeAccount(emp);
+        assertEquals(0, searchableDatabase.getUsers().size());
+    }
+
+    @Test
+    public void testRemoveNullEmployer() {
+        Employer emp = null;
+        searchableDatabase.addUser(emp);
+        admin.removeAccount(emp);
+        assertEquals(0, searchableDatabase.getUsers().size());
+    }
+
+    @Test
+    public void testRegisterNormalAdmin() {
+        admin.registerAccount("Ad", "Mihn", "admin3@a.edu", "bestADMIN");
+        assertEquals(1, searchableDatabase.getUsers().size());
+    }
+
+    @Test
+    public void testRegisterSameAdminTwice() {
+        admin.registerAccount("Ad", "Mihn", "admin3@a.edu", "bestADMIN");
+        admin.registerAccount("Ad", "Mihn", "admin3@a.edu", "bestADMIN");
+        assertEquals(1, searchableDatabase.getUsers().size());
+    }
+
+    @Test
+    public void testRegisterAdminWithNullValues() {
+        String firstName = null;
+        String lastName = null;
+        String email = null;
+        String password = null;
+        admin.registerAccount(firstName, lastName, email, password);
+        assertEquals(0, searchableDatabase.getUsers().size());
+    }
+
 }
